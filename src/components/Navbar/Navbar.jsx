f488705b278bf52bf2e4f6e3b85feb315e4632bf
Marcoviscu <marcoviscuso2000@gmail.com>
2022-09-11 21:46:14 +0200

    feat:fixed Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,22 @@
 import styles from "./index.module.scss";
 import { FaReact } from 'react-icons/fa';
+import { AiOutlineSearch } from 'react-icons/ai'
+
+
 
 const Navbar = () => {
+  
   return <div className={styles.Navbar}>
-            <div><FaReact className={styles.Logo}/></div>
-            <ul className={styles.AnchorWrapper}>
-              <li><a href=''>Movies</a></li>
-              <li><a href=''>About</a></li>
-            </ul>
+            <div className={styles.Container}>
+              <FaReact className={styles.Logo}/>
+              <ul className={styles.AnchorWrapper}>
+              <li><a>Movies</a></li>
+              <li><a>About</a></li>
+              </ul>
+            </div>
+            <div className={styles.InputSearchContainer}>
+              <AiOutlineSearch className={styles.Logo}/>
+            </div>
         </div>;
 };
 
