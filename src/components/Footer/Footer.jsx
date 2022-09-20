7e3f505cac6471a9de528dce165a3dc9bcb1beed
Marcoviscu <marcoviscuso2000@gmail.com>
2022-09-20 18:24:31 +0200

    feat:changed page style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
     return (
         <div className={styles.Main}>
             <ul className={styles.Ref}>
-                <li className={styles.Link}><a href='https://github.com/Marcoviscu'><AiFillGithub/></a></li>
-                <li className={styles.Link}><a href='https://www.linkedin.com/in/marco-viscuso/'><AiFillLinkedin/></a></li>
+                <li className={styles.Link}><a href='https://github.com/Marcoviscu' target="_blank"><AiFillGithub/></a></li>
+                <li className={styles.Link}><a href='https://www.linkedin.com/in/marco-viscuso/' target="_blank"><AiFillLinkedin/></a></li>
             </ul>
             <h2>Made by Marco Viscuso</h2>
         </div>
